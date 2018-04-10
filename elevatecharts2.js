5921a6013ef770d9a03b72faae7ce4e6f55766cb
Jason Bellew <jayb@Jasons-MacBook-Pro.local>
2018-04-10 13:51:48 -0400

    style3

--- elevatecharts2.js
+++ elevatecharts2.js
@@ -1,4 +1,4 @@
-zingchart.THEME="classic";
+zingchart.THEME="default";
 
 var myChart14 = {
     type: "bar",
@@ -14,7 +14,7 @@ var myChart14 = {
         values:[5,30,21,18,59,50,28,33]
       },
       {
-        values:[33,5,18,21,33,41,29,15]
+        values:[55,5,18,21,33,41,29,15]
       }
     ]
   };
