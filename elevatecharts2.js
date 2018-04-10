aac3e64b39a16a026f98c4537640eabfd0dff9f5
Jason Bellew <jayb@Jasons-MacBook-Pro.local>
2018-04-10 13:20:38 -0400

    updating the last chart

---
+++ elevatecharts2.js
@@ -0,0 +1,19 @@
+var myChart14 = {
+    type: "bar",
+    plot:{
+      stacked:true,
+      stackType:"normal"
+    },
+    series:[
+      {
+        values:[20,40,25,50,15,45,33,34]
+      },
+      {
+        values:[5,30,21,18,59,50,28,33]
+      },
+      {
+        values:[30,5,18,21,33,41,29,15]
+      }
+    ]
+  };
+   
