0e2f91e7dff366adc0f688fd0701aa8f5aac76e1
Jason Bellew <jayb@Jasons-MacBook-Pro.local>
2018-04-11 11:20:59 -0400

    new

--- elevatecharts2.js
+++ elevatecharts2.js
@@ -1,22 +1,36 @@
 var myChart14 = {
-    type: "bar",
-    theme: "light",
-    title: {
+    "type": "bar",
+    "theme": "light",
+    "title": {
       "text": "Cost History by Service Category",
+      "fontSize": 18
     },
-    plot:{
-      stacked:true,
-      stackType:"normal"
+    "plot":{
+      "stacked":true,
+      "stackType":"normal"
     },
-    series:[
+
+    "scale-y": {
+      "max-value": 10,
+      "label": {
+        "text": "Cost"
+      }
+    },
+  
+
+
+
+
+
+    "series":[
       {
-        values:[20,40,25,50,15,45,33,34]
+        "values":[20,40,25,50,15,45,33,34,5,18,21,33,41]
       },
       {
-        values:[5,30,21,18,59,50,28,33]
+        "values":[5,30,21,18,59,50,28,33,20,40,25,50,15]
       },
       {
-        values:[44,5,18,21,33,41,29,15]
+        "values":[77,5,18,21,33,41,29,15,18,59,50,28,33]
       }
     ]
   };
