5166329491d4ed33804f5866f42466a3d7bb5a2c
Jason Bellew <jayb@Jasons-MacBook-Pro.local>
2018-04-11 14:25:42 -0400

    adding al the new values

--- elevatecharts2.js
+++ elevatecharts2.js
@@ -63,19 +63,102 @@ var myChart14 = {
 
 
 
+    "plot":{
+      "line-width":2,
+      "marker":{
+          "size":1,
+          "visible":false
+      },
+      "tooltip":{
+          "font-family":"Roboto",
+          "font-size":"15px",
+          "text":"%t <br> %v on %data-days",
+          "text-align":"left",
+          "border-radius":2,
+          "padding":10
+      }
+  },
+
+
+
+
 
     "series":[
       {
-        "values":[200,50,50,50,50,60,50,50,50,105,50,50,70]
+        "values":[200,50,50,50,50,60,50,50,50,105,50,50,70],
+        "data-days":[
+          "April 2017",
+          "May 2017",
+          "Jun 2017",
+          "Jul 2017",
+          "Aug 2017",
+          "Sep 2017",
+          "Oct 2017",
+          "Nov 2017",
+          "Dec 2017",
+          "Jan 2018",
+          "Feb 2018",
+          "Mar 2018",
+          "Apr 2018"
+        ],
+        "text":"EC2 Compute"
       },
       {
-        "values":[86,300,50,230,50,50,50,50,150,50,50,50,50]
+        "values":[86,300,50,230,50,50,50,50,150,50,50,50,50],
+        "data-days":[
+          "April 2017",
+          "May 2017",
+          "Jun 2017",
+          "Jul 2017",
+          "Aug 2017",
+          "Sep 2017",
+          "Oct 2017",
+          "Nov 2017",
+          "Dec 2017",
+          "Jan 2018",
+          "Feb 2018",
+          "Mar 2018",
+          "Apr 2018"
+        ],
+        "text":"EC2 RI Prepay"
       },
       {
-        "values":[75,100,50,50,50,50,50,50,50,50,50,120,50]
+        "values":[75,100,50,50,50,50,50,50,50,50,50,120,50],
+        "data-days":[
+          "April 2017",
+          "May 2017",
+          "Jun 2017",
+          "Jul 2017",
+          "Aug 2017",
+          "Sep 2017",
+          "Oct 2017",
+          "Nov 2017",
+          "Dec 2017",
+          "Jan 2018",
+          "Feb 2018",
+          "Mar 2018",
+          "Apr 2018"
+        ],
+        "text":"Other"
       },
       {
-        "values":[-75,0,-120,0,-25,0,50,-133,-33,0,0,-78,0]
+        "values":[-75,0,-120,0,-25,0,50,-133,-33,0,0,-78,0],
+        "data-days":[
+          "April 2017",
+          "May 2017",
+          "Jun 2017",
+          "Jul 2017",
+          "Aug 2017",
+          "Sep 2017",
+          "Oct 2017",
+          "Nov 2017",
+          "Dec 2017",
+          "Jan 2018",
+          "Feb 2018",
+          "Mar 2018",
+          "Apr 2018"
+        ],
+        "text":"EC2 Usage"
       }
     ]
   };
